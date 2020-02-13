8a5bec7ac3eb4ab9baf45cbc7ce03f9c48560d7c
r3aper2020 <cybereaper20@gmail.com>
2020-02-12 19:33:58 -0800

    Updated

--- main.py
+++ main.py
@@ -1,27 +1,97 @@
-import yaml,smtplib
+import yaml,smtplib,os
+import email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+import time
+from google_images_download import google_images_download
+
 
 
 config = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
 
+def GetTargets():
+    receivers = []
+    phonenumber = [config['Phone_Number']]
+    carrier = ['@vzwpix.com','@pm.sprint.com', '@tmomail.net', '@mms.att.net']
+    for c in carrier:
+        for num in phonenumber:
+            receivers.append(num+c)
+            print(receivers)
+    return receivers
 
-def Send():
-    sender = 'jesustakethewheel8080@gmail.com'
-    pwd = 'emoxzkqzmgegmpzy'
-    receivers = ['7196414411@vtext.com']
+def GetAttachments():
+    response = google_images_download.googleimagesdownload()
 
-    message = """From: From Person <from@fromdomain.com>
-    To: To Person <to@todomain.com>
-    Subject: SMTP e-mail test
+    arguments = {"keywords": config["Attachments"],
+                 "output_directory": "attachments/",
+                 "no_directory": True,
+                 "format": "jpg",
+                 "chromedriver": config["Chrome_Driver"], 
+                 "limit":config["Number_Of_MSGs"], 
+                 "size": "medium"}
+    try: 
+        response.download(arguments) 
+      
+    # Handling File NotFound Error     
+    except FileNotFoundError as Error:  
+        print(Error)
 
-    This is a test e-mail message.
-    """
+def SendSMS():
+    
+    receiver = GetTargets()   
+
+    message = "Hey"
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
-        server.login(sender, pwd)
-        server.sendmail(sender, receivers, message)         
+        server.login(config["Sender"], config["Sender_Pass"])
+        server.sendmail(config["Sender"], receiver, message)         
         print("Successfully sent email")
 
     except smtplib.SMTPException:
         print("Error: unable to send email")
+
+def SendMMS(attachment):
+
+    receiver = GetTargets()
+
+	###List of attachments###
+    
+    outer = MIMEMultipart()
+    outer['Subject'] = "Hey"
+
+
+	###Add the attachments to the message###
+    # for file in arr:
+    try:
+        with open('attachments/'+attachment, 'rb') as fp:
+            msg2 = MIMEBase('application', "octet-stream")
+            msg2.set_payload(fp.read())
+        encoders.encode_base64(msg2)
+        msg2.add_header('Content-Disposition', 'attachments', filename=os.path.basename(attachment))
+        outer.attach(msg2)
+    except:
+        print("Unable to open one of the attachments.")
+        raise
+
+    composed = outer.as_string()
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(config["Sender"], config["Sender_Pass"])
+    server.sendmail(config["Sender"],receiver,composed)
+    server.quit()
+    print("IMAGE SENT")
+    time.sleep(20)
+
+
+if __name__ == "__main__":
+    
+    GetAttachments()
+
+    arr = os.listdir("attachments")
+    for attachment in arr:
+        SendMMS(attachment)
